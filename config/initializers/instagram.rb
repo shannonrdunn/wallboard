16c41a670a70fb32fa9840055ae8a850c140bfcd
shannonrdunn <shannonrdunn@gmail.com>
2013-03-10 02:40:31 +0000

    init

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,8 @@
+require "instagram"
+
+Instagram.configure do |config|
+  config.client_id = "efb5bb149cac4d329894cf5ad69ac2be"
+  config.client_secret = "5885b31c06f241a1b059900081406a4c"
+end
+
+CALLBACK_URL = "http://systemdunn.com:3000/session/callback"
